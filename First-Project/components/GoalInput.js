7f7d8924d049d35c694d0a7f4781cf6cfdf35a12
unknown <alexanderjdibuono@gmail.com>
2024-03-22 21:58:14 -0600

    Adding modal for 'add goal' and also adding close button for modal.

--- First-Project/components/GoalInput.js
+++ First-Project/components/GoalInput.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal, Image } from 'react-native';
 
 function GoalInput(props) {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -14,15 +14,25 @@ function GoalInput(props) {
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Your course goal!"
-        onChangeText={goalInputHandler}
-        value={enteredGoalText}
-      />
-      <Button title="Add Goal" onPress={addGoalHandler} />
-    </View>
+    <Modal visible={props.visible} animationType='slide'>
+      <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require('../assets/images/goal.png')}/>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button title="Add Goal" onPress={addGoalHandler} />
+        </View>
+        <View style={styles.button}>
+          <Button title="Cancel" onPress={props.onCancel}/>
+        </View>
+      </View>
+      </View>
+    </Modal>
   );
 }
 
@@ -31,18 +41,28 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
+    padding: 16,
+    backgroundColor: '#311b6b'
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
     width: '70%',
-    marginRight: 8,
     padding: 8,
   },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  button: {
+    marginTop: 20,
+    width: 100,
+    marginHorizontal: 8
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20
+  }
 });
